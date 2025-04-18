b867b8fd54af1673ffcc221543925c2a3a9e0da4
isaacisrael <email.iisrael@gmail.com>
2025-04-18 05:35:27 +0000

    feat: add `mixPepper` to `password` model

--- models/password.js
+++ models/password.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 
 async function hash(password) {
   const rounds = getNumberOfRound();
-  return await bcrypt.hash(password, rounds);
+  return await bcrypt.hash(mixPepper(password), rounds);
 }
 
 function getNumberOfRound() {
@@ -15,8 +15,18 @@ function getNumberOfRound() {
   return rounds;
 }
 
+function mixPepper(password) {
+  const pepper = process.env.PEPPER;
+  const peperSize = Math.round(pepper.length / 2);
+
+  const frontPepper = pepper.slice(0, peperSize);
+  const backPepper = pepper.slice(peperSize, pepper.length);
+
+  return frontPepper + password + backPepper;
+}
+
 async function compare(providerPassword, storePassword) {
-  return bcrypt.compare(providerPassword, storePassword);
+  return bcrypt.compare(mixPepper(providerPassword), storePassword);
 }
 
 const password = {
